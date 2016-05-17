db20ec7647f069a55a3278dbe9f79603612ecf31
Sirko Sittig <sirko.sittig@gmail.com>
2016-05-17 19:23:56 -0300

    simplify routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :tweets, only: [] do
-        collection do
-          get ':keyword', action: :index
-        end
-      end
+      resources :tweets, only: [:index]
     end
   end
 end
