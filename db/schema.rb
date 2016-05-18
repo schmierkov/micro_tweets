c5d1a2431b5170cb0696ab8065b51ebf8ae8a057
Sirko Sittig <sirko.sittig@gmail.com>
2016-05-18 14:19:59 -0300

    remove pg extension from schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160517011742) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "tweets", force: :cascade do |t|
     t.string   "tweet_id"
     t.string   "keyword"
